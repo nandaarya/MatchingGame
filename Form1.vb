7d6ade04e96fd05b4559797e78c04e2ea53bd04b
Nanda Arya Putra <nandaarya404@gmail.com>
2023-10-30 21:17:24 +0700

    Add dialog if we win and match all icons

--- Form1.vb
+++ Form1.vb
@@ -73,6 +73,9 @@
             secondClicked = clickedLabel
             secondClicked.ForeColor = Color.Black
 
+            ' Check to see if the player won
+            CheckForWinner()
+
             ' If the player clicked two matching icons, keep them 
             ' black and reset firstClicked and secondClicked 
             ' so the player can click another icon
@@ -107,4 +110,23 @@
         secondClicked = Nothing
 
     End Sub
+
+    Private Sub CheckForWinner()
+
+        ' Go through all of the labels in the TableLayoutPanel, 
+        ' checking each one to see if its icon is matched
+        For Each control In TableLayoutPanel1.Controls
+            Dim iconLabel = TryCast(control, Label)
+            If iconLabel IsNot Nothing AndAlso
+               iconLabel.ForeColor = iconLabel.BackColor Then Exit Sub
+        Next
+
+        ' If the loop didn't return, it didn't find 
+        ' any unmatched icons
+        ' That means the user won. Show a message and close the form
+        MessageBox.Show("You matched all the icons!", "Congratulations")
+        Close()
+
+    End Sub
+
 End Class
